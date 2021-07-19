02b593d39508393cf69fbf78976d619d9d199cac
Renyi Tan <tanrenyi@gmail.com>
2021-07-19 14:43:28 +0800

    add logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import { useState } from 'react';
 
-import { Button, Select } from '@chakra-ui/react';
+import {
+  Button,
+  Select,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from '@chakra-ui/react';
 
 function App() {
   const [numDiners, setNumDiners] = useState(0);
+  const [isChildrenAboveTwelveVaccinated, setIsChildrenAboveTwelveVaccinated] =
+    useState(false);
+  const [isChildrenBelowTwelve, setIsChildrenBelowTwleve] = useState(false);
+  const [isDinersFromSameHousehold, setIsDinersFromSameHousehold] =
+    useState(false);
+
+  const [numChildren, setNumChildren] = useState(0);
+  const [numDinersWithChildren, setnumDinersWithChildren] = useState(0);
   return (
     <div className="App">
       <div>
@@ -12,21 +28,103 @@ function App() {
           placeholder="Select Option"
           onChange={(event) => setNumDiners(event.target.value)}
         >
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-          <option value="3">Option 3</option>
+          <option value="2">1 -2 pax</option>
+          <option value="5">3 -5 pax</option>
+          <option value="6">6 pax or more</option>
         </Select>
       </div>
 
-      {numDiners > 1 && (
+      {numDiners > 2 && (
         <div>
           <h1>Is there anyone above 12 years old fully vaccinated</h1>
-          <Select placeholder="Select Option">
-            <option value="option1">Yes</option>
-            <option value="option2">No</option>
+          <Select
+            placeholder="Select Option"
+            onChange={(event) =>
+              setIsChildrenAboveTwelveVaccinated(
+                event.target.value === 'yes' ?? false
+              )
+            }
+          >
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
           </Select>
         </div>
       )}
+
+      {numDiners > 2 && numDiners < 6 && isChildrenAboveTwelveVaccinated && (
+        <div>
+          <h1>Are there any children 12 years old and below?</h1>
+          <Select
+            placeholder="Select Option"
+            onChange={(event) =>
+              setIsChildrenBelowTwleve(event.target.value === 'yes' ?? false)
+            }
+          >
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </Select>
+        </div>
+      )}
+
+      {numDiners > 2 &&
+        numDiners < 6 &&
+        isChildrenAboveTwelveVaccinated &&
+        isChildrenBelowTwelve && (
+          <div>
+            <h1>Are all diners from the same household?</h1>
+            <Select
+              placeholder="Select Option"
+              onChange={(event) =>
+                setIsDinersFromSameHousehold(
+                  event.target.value === 'yes' ?? false
+                )
+              }
+            >
+              <option value="yes">Yes</option>
+              <option value="no">No</option>
+            </Select>
+          </div>
+        )}
+
+      {numDiners > 2 &&
+        numDiners < 6 &&
+        isChildrenAboveTwelveVaccinated &&
+        isChildrenBelowTwelve && (
+          <div>
+            <h1>How many children are there?</h1>
+            <NumberInput
+              defaultValue={0}
+              onChange={(event) => setNumChildren(parseInt(event))}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </div>
+        )}
+
+      {numDiners > 2 &&
+        numDiners < 6 &&
+        isChildrenAboveTwelveVaccinated &&
+        isChildrenBelowTwelve &&
+        numChildren === 2 && (
+          <div>
+            <h1>How many diners in the group?</h1>
+            <Select
+              placeholder="Select Option"
+              onChange={(event) =>
+                setnumDinersWithChildren(parseInt(event.target.value))
+              }
+            >
+              <option value="3">3 diners</option>
+              <option value="4">4 or 5 diners</option>
+            </Select>
+          </div>
+        )}
+
+      <Button>Check</Button>
     </div>
   );
 }
