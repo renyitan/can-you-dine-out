3493ec5af4a0b8a20d076e34bb78ec4e62756f1c
Renyi Tan <tanrenyi@gmail.com>
2021-07-19 17:32:23 +0800

    add validator for state changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import { useEffect, useState } from 'react';
 
 import {
-  Box,
   Button,
   ButtonGroup,
-  Container,
   Center,
-  Flex,
-  Grid,
-  GridItem,
   Select,
   Stack,
+  Heading,
 } from '@chakra-ui/react';
 
 function App() {
@@ -22,24 +18,38 @@ function App() {
     useState(false);
   const [numChildren, setNumChildren] = useState(1);
   const [numDinersWithChildren, setnumDinersWithChildren] = useState(0);
-  const [isAllowed, setIsAllowed] = useState(true);
+  const [isAllowed, setIsAllowed] = useState('');
+  const [resultSnapshot, setResultSnapshot] = useState({});
 
   function reset() {
+    setIsAllowed('');
     setNumDiners(2);
     setIsChildrenAboveTwelveVaccinated(false);
     setIsChildrenBelowTwleve(false);
     setIsDinersFromSameHousehold(false);
     setNumChildren(1);
     setnumDinersWithChildren(0);
+    setResultSnapshot({});
   }
 
   useEffect(() => {
-    if (numDiners === 2) reset();
+    if (numDiners === 2 || numDiners === 6) reset();
   }, [numDiners]);
 
   useEffect(() => {
     setNumChildren(1);
   }, [isDinersFromSameHousehold]);
+
+  useEffect(() => {
+    setIsAllowed('');
+  }, [
+    numDiners,
+    isChildrenAboveTwelveVaccinated,
+    isChildrenBelowTwelve,
+    isDinersFromSameHousehold,
+    numChildren,
+    numDinersWithChildren,
+  ]);
 
   function check() {
     if (numDiners <= 2) {
@@ -85,10 +95,35 @@ function App() {
 
   return (
     <div>
-      <Center>
-        <Stack>
+      <Center marginX={10} marginY={5}>
+        <Stack spacing={6}>
           <div>
-            <h1>Number of diners</h1>
+            <Center
+              bg={
+                isAllowed
+                  ? 'green.200'
+                  : isAllowed === ''
+                  ? 'gray.200'
+                  : 'red.200'
+              }
+              paddingY={4}
+              marginY={2}
+            >
+              <Heading as="h5" size="sm">
+                {isAllowed
+                  ? 'Allowed'
+                  : isAllowed === ''
+                  ? "Please press 'Check' to see results"
+                  : 'Not Allowed'}
+              </Heading>
+            </Center>
+          </div>
+
+          <div>
+            <Heading as="h5" size="sm">
+              Number of diners
+            </Heading>
+
             <Select
               value={numDiners}
               onChange={(event) => setNumDiners(parseInt(event.target.value))}
@@ -100,7 +135,10 @@ function App() {
           </div>
 
           <div>
-            <h1>Is there anyone above 12 years old fully vaccinated</h1>
+            <Heading as="h5" size="sm">
+              Is there anyone above 12 years old fully vaccinated
+            </Heading>
+
             <Select
               value={isChildrenAboveTwelveVaccinated ? 'yes' : 'no'}
               disabled={numDiners <= 2 || numDiners >= 6}
@@ -116,10 +154,16 @@ function App() {
           </div>
 
           <div>
-            <h1>Are there any children 12 years old and below?</h1>
+            <Heading as="h5" size="sm">
+              Are there any children 12 years old and below?
+            </Heading>
             <Select
               value={isChildrenBelowTwelve ? 'yes' : 'no'}
-              disabled={numDiners <= 2 || !isChildrenAboveTwelveVaccinated}
+              disabled={
+                numDiners <= 2 ||
+                numDiners >= 6 ||
+                !isChildrenAboveTwelveVaccinated
+              }
               onChange={(event) =>
                 setIsChildrenBelowTwleve(event.target.value === 'yes' ?? false)
               }
@@ -130,11 +174,14 @@ function App() {
           </div>
 
           <div>
-            <h1>Are all diners from the same household?</h1>
+            <Heading as="h5" size="sm">
+              Are all diners from the same household?
+            </Heading>
             <Select
               value={isDinersFromSameHousehold ? 'yes' : 'no'}
               disabled={
                 numDiners <= 2 ||
+                numDiners >= 6 ||
                 !isChildrenAboveTwelveVaccinated ||
                 !isChildrenBelowTwelve
               }
@@ -150,11 +197,14 @@ function App() {
           </div>
 
           <div>
-            <h1>How many children are there?</h1>
+            <Heading as="h5" size="sm">
+              How many children are there?
+            </Heading>
             {isDinersFromSameHousehold && (
               <Select
                 disabled={
                   numDiners <= 2 ||
+                  numDiners >= 6 ||
                   !isChildrenAboveTwelveVaccinated ||
                   !isChildrenBelowTwelve
                 }
@@ -172,6 +222,7 @@ function App() {
               <Select
                 disabled={
                   numDiners <= 2 ||
+                  numDiners >= 6 ||
                   !isChildrenAboveTwelveVaccinated ||
                   !isChildrenBelowTwelve
                 }
@@ -189,11 +240,14 @@ function App() {
 
           {!isDinersFromSameHousehold && numChildren === 2 && (
             <div>
-              <h1>How many diners in the group?</h1>
+              <Heading as="h5" size="sm">
+                How many diners in the group?
+              </Heading>
               <Select
                 value={numDinersWithChildren}
                 disabled={
                   numDiners <= 2 ||
+                  numDiners >= 6 ||
                   !isChildrenAboveTwelveVaccinated ||
                   !isChildrenBelowTwelve
                 }
@@ -207,18 +261,14 @@ function App() {
             </div>
           )}
           <ButtonGroup>
-            <Button onClick={() => check()}>Check</Button>
-            <Button onClick={() => reset()}>Reset</Button>
+            <Button onClick={() => check()} variant="outline">
+              Check
+            </Button>
+            <Button onClick={() => reset()} variant="outline">
+              Reset
+            </Button>
           </ButtonGroup>
         </Stack>
-
-        <div>
-          {isAllowed ? (
-            <Box bg="green.200">Allowed</Box>
-          ) : (
-            <Box bg="red.200">Not Allowed</Box>
-          )}
-        </div>
       </Center>
     </div>
   );
